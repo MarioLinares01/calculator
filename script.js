e48473f1dbbc56da7c12124b87e34ffb07991a46
Mario Linares <mariolinares109@gmail.com>
2024-03-22 21:25:40 -0700

    Add operator functions

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log("Hello from the script")
 });
 
-// Math Functions
+
 const add = function (a, b) {
     return a + b;
 };
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    return a / b;
+}
