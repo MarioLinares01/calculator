56fb246ccb21809d0cc78fbc35ddc74e14932d87
Mario Linares <mariolinares109@gmail.com>
2024-04-01 11:13:49 -0700

    Add operate function

--- script.js
+++ script.js
@@ -1,26 +1,48 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log("Hello from the script")
-});
+const display = document.querySelector('.display')
+const clear = document.querySelector('.clear');
+const numbers = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+const equal = document.querySelector('.equal');
 
 
+// Additon function
 const add = function (a, b) {
     return a + b;
 };
 
+// Subraction function
 const subtract = function (a, b) {
     return a - b;
 };
 
+// Multiplication function
 const multiply = function (a, b) {
     return a * b;
 };
 
+// Division function
 const divide = function (a, b) {
     return a / b;
 };
 
 // Function to display the result
-const display = function (result) {
-    let display = document.querySelector('.display');
+const updateDisplay = function (result) {
     display.textContent = result
 };
+
+// Clear Display
+const clearDisplay = function () {
+    display.textContent = '';
+}
+
+const operate = function(num1, num2, operator) {
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === '*') {
+        return multiply(num1, num2);
+    } else if (operator === '/') {
+        return divide(num1, num2);
+    }
+}
